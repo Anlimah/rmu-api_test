c215f1fbca88728575573966326bc54a54e4031c
Anlimah <y.m.ratty7@gmail.com>
2023-06-30 15:53:40 +0000

    Updating authwntication code 5

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
         // Get the authorization header
         $authorizationHeader = isset($_SERVER['HTTP_AUTHORIZATION']) ? $_SERVER['HTTP_AUTHORIZATION'] : '';
-        echo json_encode(array("success" => true, "message" => getallheaders())); // Example response
+        echo json_encode(array("success" => true, "message" => $authorizationHeader)); // Example response
         exit;
         // Extract the username and password from the authorization header
         $credentials = null;
