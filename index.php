aa155fbe355a91ea8e55539558fe6b45faa680bc
Anlimah <y.m.ratty7@gmail.com>
2023-06-30 15:02:59 +0000

    worked on authorization

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 require_once('bootstrap.php');
 
@@ -8,100 +10,44 @@ use Src\Controller\ExposeDataController;
 
 switch ($_SERVER["REQUEST_METHOD"]) {
     case 'POST':
-
+        
         // Get the authorization header
         $authorizationHeader = isset($_SERVER['HTTP_AUTHORIZATION']) ? $_SERVER['HTTP_AUTHORIZATION'] : '';
-
-        // Extract the secret key from the authorization header
-        $secretKey = null;
-        if (preg_match('/Bearer\s+(.*)$/i', $authorizationHeader, $matches)) {
-            $secretKey = $matches[1];
+        echo json_encode(array("success" => true, "message" => $authorizationHeader)); // Example response
+        exit;
+        // Extract the username and password from the authorization header
+        $credentials = null;
+        if (preg_match('/Basic\s+(.*)$/i', $authorizationHeader, $matches)) {
+            $credentials = base64_decode($matches[1]);
         }
 
-        if ($secretKey) {
-            // Retrieve the client ID and signature from the secret key
-            list($clientId, $signature) = explode(':', $secretKey);
+        if ($credentials) {
+            // Extract the username and password from the credentials
+            list($username, $password) = explode(':', $credentials);
 
-            // Verify the signature against the payload and client secret (fetch the client secret from the database)
-            $clientSecret = $expose->getSecretKeyFromDatabase($clientId); // Implement your logic to fetch the client secret from the database
+            // Validate the username and password (fetch the expected username and password from the database)
+            $expectedUsername = "Francis"; // Implement your logic to fetch the expected username from the database
+            $expectedPassword = "Anlimah"; // Implement your logic to fetch the expected password from the database
 
-            $payload = json_encode(array(
-                "amount" => $data["amount"],
-                "callback_url" => $callback_url,
-                "customer_number" => $data["phone_number"],
-                "exttrid" => $trans_id,
-                "nw" => $data["network"],
-                "reference" => "RMU Forms Online",
-                "service_id" => getenv('ORCHARD_SERVID'),
-                "trans_type" => "CTM",
-                "ts" => date("Y-m-d H:i:s")
-            ));
-
-            $expectedSignature = hash_hmac("sha256", $payload, $clientSecret);
-
-            if ($signature === $expectedSignature) {
+            if ($username === $expectedUsername && $password === $expectedPassword) {
                 $_POST = json_decode(file_get_contents("php://input"), true);
-                $response = array();
 
-                if (!empty($_POST)) {
-
-                    $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2)[0];
-                    $endpoint = '/' . basename($request_uri);
-
-
-                    switch ($endpoint) {
-
-                        case 'forms':
-                            break;
-
-                        case 'buy':
-                            break;
-
-                        case 'status':
-                            break;
-
-                        case 'ussd':
-
-                            $payData = array();
-
-                            if (!empty($_POST)) $response = (new USSDHandler($_POST))->run();
-
-                            if (isset($response["data"])) $payData = $response["data"];
-
-                            if (!empty($payData)) {
-                                sleep(8);
-                                (new PaymentController())->orchardPaymentControllerB($payData);
-                            }
-
-                            break;
-
-                        case 'confirm':
-
-                            $expose = new ExposeDataController();
-                            $expose->requestLogger(json_encode($_POST));
-
-                            if (!empty($_POST)) {
-                                $transaction_id = $expose->validatePhone($_POST["trans_ref"]);
-                                $data = $expose->confirmPurchase($transaction_id);
-                                $expose->requestLogger($transaction_id . " - " . $data[0]["message"]);
-                            }
-
-                            break;
-
-                        default:
-                            # code...
-                            break;
-                    }
-                }
+                // Continue with the rest of your code here
+                header("Content-Type: application/json");
+                http_response_code(201);
+                echo json_encode(array("success" => true, "message" => "Authorized")); // Example response
+                exit;
             } else {
-                // The signature is invalid, send an appropriate response message
+                // The username or password is invalid, send an appropriate response message
                 http_response_code(401); // Unauthorized
-                echo json_encode(array("message" => "Invalid signature"));
+                header('WWW-Authenticate: Basic realm="API Authentication"');
+                echo json_encode(array("message" => "Invalid credentials"));
                 exit;
             }
         } else {
             // The authorization header is missing or invalid, send an appropriate response message
             http_response_code(401); // Unauthorized
+            header('WWW-Authenticate: Basic realm="API Authentication"');
             echo json_encode(array("message" => "Authorization required"));
             exit;
         }
