7c02791f0f1b1a836f25a43bb49bd25d9d96d9bd
Anlimah <y.m.ratty7@gmail.com>
2023-11-08 17:11:04 +0000

    external transaction length checker corrected 13

--- src/Controller/ExposeDataController.php
+++ src/Controller/ExposeDataController.php
@@ -244,7 +244,7 @@ class ExposeDataController
     public function fetchCompanyIDByCode($companyCode, $apiUser): mixed
     {
         $query = "SELECT vd.`id` FROM vendor_details AS vd, api_users AS au 
-                WHERE vd.`company_code` = {$companyCode} AND au.id = {$apiUser} AND branch = 'MAIN' AND au.vendor_id = vd.id";
-        return $query; //$this->dm->getID($query, array(":c" => $companyCode, ":a" => $apiUser));
+                WHERE vd.`company_code` = :c AND au.id = :a AND branch = 'MAIN' AND au.vendor_id = vd.id";
+        return $this->dm->getID($query, array(":c" => $companyCode, ":a" => $apiUser));
     }
 }
